0b158d20718b9317b92b2daf8a1efb2dc1bfd054
Nikita <niklog21@gmail.com>
2022-04-09 15:26:04 +0300

    Changed themes assets

--- app/src/main/java/com/holographic/call/utils/Constants.kt
+++ app/src/main/java/com/holographic/call/utils/Constants.kt
@@ -7,20 +7,20 @@ import com.holographic.call.model.theme.VideoTheme
 
 val defaultTheme = ImageTheme("file:///android_asset/theme_placeholder.webp")
 val audioThemesIndexes = List(6) { it }
-val themesPopular: List<Theme> = List(6) {
+private val themesPopular: List<Theme> = List(6) {
     VideoTheme(
         "file:///android_asset/popular/videos/$it.mp4",
         "file:///android_asset/popular/gifs/$it.gif",
         it in audioThemesIndexes
     )
 }
-val themesGames: List<Theme> = List(6) {
+private val themesGames: List<Theme> = List(6) {
     ImageTheme("file:///android_asset/games/$it.webp")
 }
-val themesCats: List<Theme> = List(6) {
+private val themesCats: List<Theme> = List(6) {
     ImageTheme("file:///android_asset/cats/$it.webp")
 }
-val themesMovies: List<Theme> = List(6) {
+private val themesMovies: List<Theme> = List(6) {
     ImageTheme("file:///android_asset/movies/$it.webp")
 }
 val presetThemes: List<Theme> = listOf(
