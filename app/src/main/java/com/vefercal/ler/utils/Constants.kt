8daa424904f1ee9fa11556a020bc6d09badcf91f
Nikita <niklog21@gmail.com>
2022-12-02 17:53:43 +0200

    Changed assets

--- app/src/main/java/com/vefercal/ler/utils/Constants.kt
+++ app/src/main/java/com/vefercal/ler/utils/Constants.kt
@@ -1,30 +1,18 @@
 package com.vefercal.ler.utils
 
 import com.vefercal.ler.App
-import com.vefercal.ler.model.theme.ImageTheme
 import com.vefercal.ler.model.theme.Theme
 import com.vefercal.ler.model.theme.VideoTheme
 
-val videoThemes = List(6) {
+val videoThemes = List(8) {
     VideoTheme(
-        "file:///android_asset/popular/video/$it.mp4",
-        "file:///android_asset/popular/gif/$it.gif",
+        "file:///android_asset/videos/vid_${it + 1}.mp4",
+        "file:///android_asset/gifs/$it.gif",
         true
     )
 }
-val themesNew = List(8) {
-    ImageTheme("file:///android_asset/new/$it.png")
-}
-val themesTop = List(8) {
-    ImageTheme("file:///android_asset/top/$it.png")
-}
-val themesPopular: List<Theme> = MutableList<Theme>(8) {
-    ImageTheme("file:///android_asset/popular/image/$it.png")
-}.apply { addAll(0, videoThemes) }.toList()
 val presetThemes: List<Theme> = listOf(
-    *themesPopular.toTypedArray(),
-    *themesTop.toTypedArray(),
-    *themesNew.toTypedArray()
+    *videoThemes.toTypedArray()
 )
 
 val appLink: String by lazy { "https://play.google.com/store/apps/details?id=" + App.instance.packageName }
