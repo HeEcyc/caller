e9a330f1bb4a27beb9a9832cbef79536752ed1df
Nikita <niklog21@gmail.com>
2023-04-04 17:38:59 +0300

    Changed assets

--- app/src/main/java/com/bbbotttixxx/callscreen/utils/Constants.kt
+++ app/src/main/java/com/bbbotttixxx/callscreen/utils/Constants.kt
@@ -3,9 +3,9 @@ package com.bbbotttixxx.callscreen.utils
 import com.bbbotttixxx.callscreen.model.theme.Theme
 import com.bbbotttixxx.callscreen.model.theme.VideoTheme
 
-val videoThemes = List(8) {
+val videoThemes = List(4) {
     VideoTheme(
-        "file:///android_asset/videos/vid_${it + 1}.mp4",
+        "file:///android_asset/videos/vid_$it.mp4",
         "file:///android_asset/gifs/$it.gif",
         true
     )
