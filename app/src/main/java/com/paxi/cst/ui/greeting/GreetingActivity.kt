d1b12991506a26a8cdd884bf28d6df555ed918d7
Nikita <niklog21@gmail.com>
2023-04-12 03:25:26 +0300

    Changed assets

--- app/src/main/java/com/paxi/cst/ui/greeting/GreetingActivity.kt
+++ app/src/main/java/com/paxi/cst/ui/greeting/GreetingActivity.kt
@@ -3,6 +3,7 @@ package com.paxi.cst.ui.greeting
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.postDelayed
 import com.paxi.cst.R
 
 class GreetingActivity : AppCompatActivity(R.layout.greeting_activity) {
@@ -10,6 +11,9 @@ class GreetingActivity : AppCompatActivity(R.layout.greeting_activity) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         findViewById<View>(R.id.root)?.setOnClickListener {
+            onBackPressed()
+        }
+        findViewById<View>(R.id.root)?.postDelayed(2000) {
             onBackPressed()
         }
     }
